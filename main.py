9e40fbac6a2d2a188574777360071779495ccdb3
Gnana Prakash <sanjayprakashsm@gmail.com>
2021-04-04 11:55:30 +0530

    speech to text

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request
 from utils.speechToText import get_large_audio_transcription
 from utils.base64Tomp3 import base64Tomp3
 import os
@@ -6,15 +6,17 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/speechToText')
+@app.route('/speechToText',methods=['POST'])
 def decoding_mp3():
-    mp3_file = open(os.getcwd()+'/files/temp.mp3','wb')
-    decode_string = base64Tomp3(url='http://localhost:8000/')
-    mp3_file.write(decode_string)
-    path = 'files/temp.mp3'
-    full_text = get_large_audio_transcription(path)
-    return jsonify({"text":full_text})
-
+    if request.method == 'POST':
+        encoded_string = request.json['base64']
+        mp3_file = open(os.getcwd()+'/files/temp.mp3','wb')
+        decode_string = base64Tomp3(encoded_string)
+        mp3_file.write(decode_string)
+        path = 'files/temp.mp3'
+        full_text = get_large_audio_transcription(path)
+        return jsonify({"text":full_text})
+    return jsonify({"error":"Something went wrong"})
 
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=5001,debug=False)
--- utils/base64Tomp3.py
+++ utils/base64Tomp3.py
@@ -3,9 +3,7 @@ import os
 import requests
 
 
-def base64Tomp3(url):
-    response = requests.get(url)
-    encoded_string = response.json()['base64']
+def base64Tomp3(encoded_string):
     decode_string = base64.b64decode(encoded_string.encode('utf-8'))
     print(type(decode_string))
     return decode_string
